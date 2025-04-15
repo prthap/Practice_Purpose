64c6ecbd1bbf247f6903ffa3bed96185112133a4
Prathap S <prathapmudigere@gmail.com>
2025-04-15 15:14:20 +0530

    first comment

---
+++ YourTread.java
@@ -0,0 +1,27 @@
+/*public class YourTread{
+    void task(){
+        for (int i=0;i<10;i++)
+        {
+            System.out.println("Thread started....1");
+
+        }
+        
+
+    }
+    
+}
+
+
+public class Tre{
+    public static void main (String[] args) {
+        System.out.println("..Application is Started.....");
+        YourTread you = new YourTread();
+        you.task();
+        System.out.println("..Application is Ended.....");
+        
+        
+        
+        
+        
+    }
+}
