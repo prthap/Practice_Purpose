84960b3a3733a7ecac9728f5b37b7b0cc1c1472d
prathap <prathapmudigere@gmail.com>
2025-04-17 12:17:15 +0530

    third commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum{
+    
+    public static void main(String power[]){
+        int summ = 0;
+        for(int i=1;i<=5;i++){
+            summ = summ + i;
+
+        }
+        System.out.println(summ);
+    }
+}
